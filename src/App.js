bad004ee7cd31ffc0a71783a0ac3a5acbee306d6
noveshgoyal <goyalnovesh@gmail.com>
2022-09-23 20:01:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [output, setOutput] = useState("");
+  const [dob, setDob] = useState("");
+  const [luckyNum, setLuckyNum] = useState("");
+
+  function getDOB(event) {
+    setDob(event.target.value);
+  }
+
+  function getLuckyNum(event) {
+    setLuckyNum(event.target.value);
+  }
+
+  function calculateSum(dob) {
+    let sum = 0;
+    const dateOfBirth = dob.replaceAll("-", "");
+    for (let num of dateOfBirth) {
+      sum = sum + Number(num);
+    }
+    return sum;
+  }
+
+  function checkNumber(dob, luckyNumber) {
+    if (dob || luckyNumber === "") {
+      alert("Please Enter both the Numbers!");
+    } else {
+      const sum = calculateSum(dob);
+      if (sum % luckyNumber === 0) {
+        setOutput("Your Bithdate is very Lucky!🥳");
+      } else {
+        setOutput("Your birthdate is not Lucky!😞");
+      }
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Is Your Birthdate Lucky?🥳</h1>
+        <div id="dob-container">
+          <label htmlFor="dob">Enter Your Date of Birth:</label>
+          <br />
+          <input type="date" id="dob" onChange={getDOB}></input>
+        </div>
+        <div id="luckyNum-container">
+          <label htmlFor="lucky-num">What's your Lucky Number:</label>
+          <br />
+          <input type="number" id="lucky-num" onChange={getLuckyNum}></input>
+        </div>
+        <button onClick={() => checkNumber(dob, luckyNum)}>Check</button>
+        <div id="output">{output}</div>
+      </div>
     </div>
   );
 }
